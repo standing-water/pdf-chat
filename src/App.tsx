38e637ba740beecd423c46f8efd1b089b67d57b4
ShinHyunJong <kashiashin@gmail.com>
2019-11-08 22:19:10 +0900

    Pdf pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { Document, Page } from "react-pdf/dist/entry.webpack";
 // import pdf from "./sample.pdf";
 
@@ -13,10 +13,27 @@ const App: React.FC = () => {
   // const onDocumentLoadSuccess = () => {
   //   console.log("Test");
   // };
+  const [pageNumber, setPageNumber] = useState(1);
+  const [_numPages, setNumPages] = useState(null);
+
+  const onDocumentLoadSuccess = ({ numPages }: { numPages: any }) => {
+    console.log("success");
+    setNumPages(numPages);
+  };
+
+  const goToPrevPage = () => {
+    setPageNumber(pageNumber - 1);
+  };
+  const goToNextPage = () => {
+    setPageNumber(pageNumber + 1);
+  };
 
   return (
-    <div className='App'>
-      test
+    <div className="App">
+      <nav>
+        <button onClick={goToPrevPage}>Prev</button>
+        <button onClick={goToNextPage}>Next</button>
+      </nav>
       <Document
         file={{
           url: "http://localhost:7070"
@@ -27,11 +44,11 @@ const App: React.FC = () => {
           // },
           // withCredentials: true
         }}
-        onLoadError={(err) => console.log(err)}
+        onLoadError={err => console.log(err)}
         // options={options}
-        // onLoadSuccess={onDocumentLoadSuccess}
+        onLoadSuccess={onDocumentLoadSuccess}
       >
-        <Page pageNumber={1} />
+        <Page pageNumber={pageNumber} />
       </Document>
     </div>
   );
