b1f49475b6bb6063f45dd3ec5b2b84cb8239ad4f
Yunhwan Na <nayunhwan.dev@gmail.com>
2019-11-09 16:48:39 +0900

    update brach mobile

--- src/apis/presentation.ts
+++ src/apis/presentation.ts
@@ -2,19 +2,16 @@ import axios from "axios";
 import { API_URL } from "constants/server";
 
 export const getPresentation = () => {};
-export const createPresentation = (name: string, file: File) =>
-  axios.post(
-    `${API_URL}/presentation`,
-    {
-      name,
-      file
-    },
-    {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,OPTIONS",
-        Accept: "application/json",
-        "Content-Type": "application/json"
-      }
+export const createPresentation = (name: string, file: File) => {
+  const formData = new FormData();
+  formData.append("name", name);
+  formData.append("file", file);
+  return axios.post(`${API_URL}/presentation`, formData, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,OPTIONS",
+      Accept: "application/json",
+      "Content-Type": "multipart/form-data"
     }
-  );
+  });
+};
