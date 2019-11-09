e263cb416cb6f663cd78f732487d45de401ec604
ShinHyunJong <kashiashin@gmail.com>
2019-11-09 13:03:55 +0900

    wip:: main page

--- src/containers/mainPage.tsx
+++ src/containers/mainPage.tsx
@@ -6,7 +6,11 @@ import {
   PdfWrapper,
   ChatWrapper,
   TabWrapper,
-  TabItem
+  TabItem,
+  ChatContentWrapper,
+  InputWrapper,
+  PdfFooter,
+  PdfContentWrapper
 } from "./mainPageStyle";
 
 interface Props {}
@@ -37,24 +41,34 @@ export const MainPage: React.FC<Props> = ({}) => {
           <button onClick={goToPrevPage}>Prev</button>
           <button onClick={goToNextPage}>Next</button>
         </nav>
-        <Document
-          file={{
-            url: "http://localhost:7070"
-            // httpHeaders: {
-            //   "Access-Control-Request-Method": "GET",
-            //   "Access-Control-Allow-Origin": "*",
-            //   "X-CustomHeader": "40359820958024350238508234"
-            // },
-            // withCredentials: true
-          }}
-          onLoadError={err => console.log(err)}
-          // options={options}
-          onLoadSuccess={onDocumentLoadSuccess}
-        >
-          <Page pageNumber={pageNumber} />
-        </Document>
+        <PdfContentWrapper>
+          <Document
+            file={{
+              url: "http://localhost:7070"
+              // httpHeaders: {
+              //   "Access-Control-Request-Method": "GET",
+              //   "Access-Control-Allow-Origin": "*",
+              //   "X-CustomHeader": "40359820958024350238508234"
+              // },
+              // withCredentials: true
+            }}
+            onLoadError={err => console.log(err)}
+            // options={options}
+            onLoadSuccess={onDocumentLoadSuccess}
+          >
+            <Page pageNumber={pageNumber} />
+          </Document>
+        </PdfContentWrapper>
+        <PdfFooter>푸터</PdfFooter>
       </PdfWrapper>
-      <ChatWrapper></ChatWrapper>
+      <ChatWrapper>
+        <TabWrapper>
+          <TabItem>채팅</TabItem>
+          <TabItem>질문</TabItem>
+        </TabWrapper>
+        <ChatContentWrapper>ㄹ</ChatContentWrapper>
+        <InputWrapper>인풋</InputWrapper>
+      </ChatWrapper>
     </MainPageWrapper>
   );
 };
--- src/containers/mainPageStyle.ts
+++ src/containers/mainPageStyle.ts
@@ -4,25 +4,56 @@ export const MainPageWrapper = styled.div`
   display: flex;
   flex-direction: row;
   border: 5px red solid;
+  width: 100vw;
+  height: 100vh;
 `;
 
 export const PdfWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
   flex: 3;
   border: 3px blue solid;
+  height: 100%;
+`;
+
+export const PdfContentWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  overflow-y: auto;
+  border: 3px black solid;
+`;
+
+export const PdfFooter = styled.div`
+  height: 70px;
+  border: 3px fuchsia solid;
 `;
 
 export const ChatWrapper = styled.div`
+  display: flex;
   border: 3px yellow solid;
   flex: 1;
+  flex-direction: column;
+`;
+
+export const ChatContentWrapper = styled.div`
+  flex: 10;
+`;
+export const InputWrapper = styled.div`
+  flex: 1;
+  border: 2px solid brown;
 `;
 
 export const TabWrapper = styled.div`
+  flex: 1;
   display: flex;
   flex-direction: row;
   width: 100%;
 `;
 
 export const TabItem = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   flex: 1;
   border: 2px solid green;
 `;
