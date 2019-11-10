183ed6aef7f602eedba4dc1edda50818cc114bef
Yunhwan Na <nayunhwan.dev@gmail.com>
2019-11-10 11:25:45 +0900

    change url

--- src/apis/qrcode.ts
+++ src/apis/qrcode.ts
@@ -1,2 +1,2 @@
 export const getQRCode = (url: string) =>
-  `https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=http://localhost:3000/presentation/${url}`;
+  `https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=http://standingwater.jadekim.kr/presentation/${url}`;
--- src/constants/server.ts
+++ src/constants/server.ts
@@ -1,3 +1,3 @@
 // export const API_URL = "http://192.168.43.154:8080";
-export const API_URL = "http://standingwater.jadekim.kr";
-export const WS_URL = "ws://standingwater.jadekim.kr/realtime";
+export const API_URL = "http://api.standingwater.jadekim.kr";
+export const WS_URL = "ws://api.standingwater.jadekim.kr/realtime";
